fc97e52a2c51e27cdae7b1f0dea5d2edce0f8d39
RozArtem <rozum13art@gmail.com>
2021-05-19 12:40:35 +0300

    init

---
+++ src/main/java/backend/exam/controller/FirstController.java
@@ -0,0 +1,17 @@
+package backend.exam.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+
+
+    @GetMapping("/first")
+
+        public void first() {
+
+
+    }
+}
